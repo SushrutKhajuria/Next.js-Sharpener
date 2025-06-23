6e13ff9861f9c508728a8bd3a7a55a55e28d59c4
SushrutKhajuria <sushrutkhajuria1995@gmail.com>
2025-06-23 17:55:12 +0530

    Final touch to About US page

--- pages/aboutus.js
+++ pages/aboutus.js
@@ -1,5 +1,25 @@
-function AboutUsPage() {
-  return <h1>About Us PAGE</h1>;
-}
+import Link from 'next/link';
 
-export default AboutUsPage;
+const details = [
+  { id: 1, name: 'Yash', role: 'Senior Developer' },
+  { id: 2, name: 'Vaibhav', role: 'Backend Developer' },
+  { id: 3, name: 'Suresh', role: 'Frontend Developer' },
+];
+
+export default function AboutUsPage() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <h2>Our Team</h2>
+      <ul>
+        {details.map((dev) => (
+          <li key={dev.id}>
+            <Link href={`/aboutus/${dev.id}`}>
+              <h3>{dev.name}</h3>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
