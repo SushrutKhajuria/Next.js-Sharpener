6c8053b8775875c2377326ba0a404e46972a4a6e
SushrutKhajuria <sushrutkhajuria1995@gmail.com>
2025-06-23 16:24:53 +0530

    Making Nested Routes

---
+++ pages/aboutus/developer.js
@@ -0,0 +1,11 @@
+// File: pages/aboutus/developer.js
+function DeveloperPage() {
+  return (
+    <div>
+      <h1>About the Developer</h1>
+      <h2>Your Name: Sushrut Khajuria</h2>
+    </div>
+  );
+}
+
+export default DeveloperPage;
