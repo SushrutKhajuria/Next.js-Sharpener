8d371bd396d98b3d4c796f5b6bbf8aae341b75d3
SushrutKhajuria <sushrutkhajuria1995@gmail.com>
2025-06-24 18:07:50 +0530

    Dynamic Routing DIY

---
+++ Meetups/pages/[meetupId].js
@@ -0,0 +1,39 @@
+import { useRouter } from 'next/router';
+    import MeetupDetail from '../components/meetups/MeetupDetail';
+const DUMMY_MEETUPS = [
+  {
+    id: 'm1',
+    title: 'A First Meetup',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+    address: 'Some address 5, 12345 Some City',
+    description: 'This is a first meetup!'
+  },
+  {
+    id: 'm2',
+    title: 'A Second Meetup',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+    address: 'Some address 10, 12345 Some City',
+    description: 'This is a second meetup!'
+  }
+];
+
+function MeetupDetails() {
+  const router = useRouter();
+  const meetupId = router.query.meetupId;
+  const selectedMeetup = DUMMY_MEETUPS.find(meetup => meetup.id === meetupId);
+
+  if (!selectedMeetup) {
+    return <div>Meetup not found!</div>;
+  }
+
+  return (
+    <MeetupDetail
+      image={selectedMeetup.image}
+      title={selectedMeetup.title}
+      address={selectedMeetup.address}
+      description={selectedMeetup.description}
+    />
+  );
+}
+
+export default MeetupDetails;
