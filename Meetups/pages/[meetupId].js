49a4c60ff9ccd8558a438d87a15120574d0b393f
SushrutKhajuria <sushrutkhajuria1995@gmail.com>
2025-06-24 22:48:46 +0530

    Prerendering the powers of Next JS

--- Meetups/pages/[meetupId].js
+++ Meetups/pages/[meetupId].js
@@ -1,5 +1,6 @@
 import { useRouter } from 'next/router';
-    import MeetupDetail from '../components/meetups/MeetupDetail';
+import MeetupDetail from '../components/meetups/MeetupDetail';
+
 const DUMMY_MEETUPS = [
   {
     id: 'm1',
@@ -14,26 +15,53 @@ const DUMMY_MEETUPS = [
     image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
     address: 'Some address 10, 12345 Some City',
     description: 'This is a second meetup!'
+  },
+  {
+    id: 'm3',
+    title: 'Third Meetup',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+    address: 'Some address 15, 12345 Some City',
+    description: 'This is a third meetup!'
   }
 ];
 
-function MeetupDetails() {
+function MeetupDetails({ meetup }) {
   const router = useRouter();
-  const meetupId = router.query.meetupId;
-  const selectedMeetup = DUMMY_MEETUPS.find(meetup => meetup.id === meetupId);
 
-  if (!selectedMeetup) {
-    return <div>Meetup not found!</div>;
+  if (router.isFallback) {
+    return <div>Loading...</div>;
   }
 
   return (
     <MeetupDetail
-      image={selectedMeetup.image}
-      title={selectedMeetup.title}
-      address={selectedMeetup.address}
-      description={selectedMeetup.description}
+      image={meetup.image}
+      title={meetup.title}
+      address={meetup.address}
+      description={meetup.description}
     />
   );
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [
+      { params: { meetupId: 'm1' } },
+      { params: { meetupId: 'm2' } },
+      { params: { meetupId: 'm3' } }
+    ],
+    fallback: false
+  };
+}
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+  const selectedMeetup = DUMMY_MEETUPS.find(meetup => meetup.id === meetupId);
+
+  return {
+    props: {
+      meetup: selectedMeetup
+    }
+  };
 }
 
 export default MeetupDetails;
